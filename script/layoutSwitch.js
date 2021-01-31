5435803241bcd39e1d0f50d01934870766178ea0
Moritz Schiesser <moritz@moritzschiesser.ch>
2021-01-31 10:51:56 +0100

    rework layoutSwitch to keep selected layout persistent across refreshes

--- script/layoutSwitch.js
+++ script/layoutSwitch.js
@@ -1,9 +1,27 @@
 window.addEventListener("load", () => {
-  document.getElementById("layoutSwitch").addEventListener("change", (e) => {
-    if(e.target.checked){
-      document.getElementsByTagName("body")[0].setAttribute("layout", "view")
-    }else{
-      document.getElementsByTagName("body")[0].setAttribute("layout", "print")
+
+  const LAYOUT_VIEW = "view";
+  const LAYOUT_PRINT = "print";
+  const LAYOUT_KEY = "layout";
+
+  let targetElement = document.getElementsByTagName("body")[0];
+  let layoutSwitch = document.getElementById("layoutSwitch");
+
+  function setLayout(cssTag) {
+    localStorage.setItem(LAYOUT_KEY, cssTag);
+    targetElement.setAttribute(LAYOUT_KEY, cssTag);
+    layoutSwitch.checked = cssTag == LAYOUT_VIEW;
+  }
+
+  if (localStorage.getItem(LAYOUT_KEY) !== undefined) {
+    setLayout(localStorage.getItem(LAYOUT_KEY));
+  }
+
+  layoutSwitch.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      setLayout(LAYOUT_VIEW);
+    } else {
+      setLayout(LAYOUT_PRINT);
     }
   })
 })
